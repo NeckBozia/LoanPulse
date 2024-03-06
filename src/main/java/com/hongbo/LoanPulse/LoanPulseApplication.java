9abb19311d0b1bd97f3281e8b3d2bd3e9bd72a4b
NeckBozia <neckbozia@outlook.com>
2024-03-06 20:57:09 +0800

    Spring Boot Application Initialize

---
+++ src/main/java/com/hongbo/LoanPulse/LoanPulseApplication.java
@@ -0,0 +1,13 @@
+package com.hongbo.LoanPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hongbo/LoanPulse/LoanPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongbo.LoanPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
